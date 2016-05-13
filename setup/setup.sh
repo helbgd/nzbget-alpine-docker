b7aa10738bcf9a413e7d48fbc347a5a4dc48907e
helbgd <helmut.angerer@googlemail.com>
2016-05-13 20:46:18 +0200

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,7 +6,7 @@ apk update && apk upgrade
 # we need the real (GNU) wget, the one incl. with Alpine's busybox is lacking some options
 # install python for 
 apk add wget
-apk add pyhton
+apk add python
 
 # add a non-root user and group called "nzbget" with no password, no home dir, no shell, and gid/uid set to 1000
 addgroup -g 1000 nzbget && adduser -H -D -G nzbget -s /bin/false -u 1000 nzbget
@@ -17,7 +17,7 @@ mkdir /nzbget && mkdir /config && mkdir /downloads
 # download the testing nzbget version
 wget -O - http://nzbget.net/info/nzbget-version-linux.json | \
 sed -n "s/^.*testing-download.*: \"\(.*\)\".*/\1/p" | \
-wget --no-check-certificate -i -O /setup/nzbget-testing-bin-linux.run || \
+wget --no-check-certificate -i - -O /setup/nzbget-testing-bin-linux.run || \
 echo "*** Download failed ***"
 
 # let's install nzbget (defaults to the "/nzbget" directory)
